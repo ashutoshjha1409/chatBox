645c33663c469d5841ae18818a7abb9d1e9823d5
Ashutosh <ashutjha@deloitte.com>
2018-07-21 19:00:17 +0530

    removed db connection

--- index.js
+++ index.js
@@ -7,11 +7,11 @@ var server = app.listen('4000', function(){
     console.log('Listening on port 4000');
 });
 
-mongo.connect('mongodb://127.0.0.1:27017/mongochat', function(err, db){
+/*mongo.connect('mongodb://127.0.0.1:27017/mongochat', function(err, db){
     if(err) throw err;
     console.log('mongodb connected');
 });
-
+*/
 app.use(express.static('public'));
 
 var io = socket(server);
@@ -20,7 +20,7 @@ io.on('connection', function(socket){
     console.log('Made socket connection', socket.id);
     socket.on('chat', function(data){
         io.sockets.emit('chat', data);
-    })
+    });
 
     socket.on('typing', function(data){
         socket.broadcast.emit('typing', data);
