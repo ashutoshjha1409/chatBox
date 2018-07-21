84f10bb4e0dd0630a03076b0183b8dcc7cdbc09f
Ashutosh <ashutjha@deloitte.com>
2018-07-21 21:13:24 +0530

    db connection in progress

--- index.js
+++ index.js
@@ -1,29 +1,40 @@
 var express = require('express');
 var app = express();
 var socket = require('socket.io');
-//var mongo = require('mongodb').MongoClient;
+var mongo = require('mongodb').MongoClient;
 var port = process.env.PORT || 4000;
 var server = app.listen(port, function(){
     console.log('Listening on port ', port);
 });
 
-/*mongo.connect('mongodb://127.0.0.1:27017/mongochat', function(err, db){
-    if(err) throw err;
-    console.log('mongodb connected');
-});
-*/
 app.use(express.static('public'));
 
 var io = socket(server);
 
-io.on('connection', function(socket){
-    console.log('Made socket connection', socket.id);
-    socket.on('chat', function(data){
-        io.sockets.emit('chat', data);
-    });
+mongo.connect('mongodb://127.0.0.1:27017/mongochat', function(err, db){
+    if(err) throw err;
+    console.log('mongodb connected');
 
-    socket.on('typing', function(data){
-        socket.broadcast.emit('typing', data);
+    //connect to socket.io
+    io.on('connection', function(socket){
+        console.log('Made socket connection', socket.id);
+        let chat = db.collection('chats')
+
+        //create function to send status
+        sendStatus = function(s){
+            io.sockets.emit('status', s);
+        }
+
+        socket.on('chat', function(data){
+            io.sockets.emit('chat', data);
+        });
+
+        socket.on('typing', function(data){
+            socket.broadcast.emit('typing', data);
+        });
     });
+    
 });
+
+
 
