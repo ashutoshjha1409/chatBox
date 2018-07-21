fb544b29bcd01973e75badc8724648eed842075a
Ashutosh <ashutjha@deloitte.com>
2018-07-21 19:28:20 +0530

    script added

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 var socket = require('socket.io');
-var mongo = require('mongodb').MongoClient;
+//var mongo = require('mongodb').MongoClient;
 var port = process.env.PORT || 4000;
 var server = app.listen(port, function(){
     console.log('Listening on port ', port);
